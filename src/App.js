976ab62feae1831e0d033885ced14d7fe8dffa40
idreesalmasri <idrees.massri95@gmail.com>
2022-05-16 23:36:02 +0300

    didnot finish

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import ToDo from './components/todo.js';
+import Header from './components/header.js';
+import Footer from './components/footer/footer.js';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <>
+      <Header/>
+      <ToDo />
+      <Footer/>
+      </>
+    );
+  }
+}
