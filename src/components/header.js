ad98f78d72873864ddef6ea6405fcd91810d16f6
idreesalmasri <idrees.massri95@gmail.com>
2022-05-21 22:52:44 +0300

    rearrange the app

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,8 @@ import { SettingContext } from "../context/setting";
 import { useContext } from "react";
 export default function Header(){
     const states = useContext(SettingContext);
-    
     return(
         <>
-        
         <Navbar style={{backgroundColor:"#252A31",marginBottom:"20px",color:"#EDEFF2"}}>
             <Navbar.Group >
                 <Navbar.Heading>To Do List: {states.incomplete} items pending</Navbar.Heading>
--- src/components/todo.js
+++ src/components/todo.js
@@ -7,8 +7,6 @@ import Header from "./header.js";
 const ToDo = () => {
   const states = useContext(SettingContext);
 
-  const paginate = (pageNumber) => states.setCurrentPage(pageNumber);
-
   const indexOfLastItem = states.currentPage * states.itemsPerPages;
   const indexOfFirstItem = indexOfLastItem - states.itemsPerPages;
   const currentItem = states.list.slice(indexOfFirstItem, indexOfLastItem);
@@ -16,13 +14,12 @@ const ToDo = () => {
   useEffect(() => {
     let incompleteCount = states.list.filter((item) => !item.complete).length;
     states.setIncomplete(incompleteCount);
-    // document.title = `To Do List: ${incomplete}`;
   }, [states.list]);
 
   return (
     <>
       <div style={{ width: "70%", margin: "auto" }}>
-        <Header/>
+        <Header />
         <div style={{ display: "flex" }}>
           <Form />
           <div style={{ width: "100%" }}>
@@ -30,11 +27,7 @@ const ToDo = () => {
           </div>
         </div>
       </div>
-      <Pagination
-        totalItems={states.list.length}
-        paginate={paginate}
-        currentPage={states.currentPage}
-      />
+      <Pagination />
     </>
   );
 };
